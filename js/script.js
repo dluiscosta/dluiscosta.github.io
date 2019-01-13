277510ff49efba00f41d53c4453e633bf8063e78
Daniel Costa <dluiscosta@gmail.com>
2019-01-13 02:01:28 -0200

    fixed js sometimes causing line break after new hyphen

--- js/script.js
+++ js/script.js
@@ -6,10 +6,21 @@ function hyphenate_titles() {
     for (i = 3; i < my_titles_lis.length; i += 2) {
       var previous_title = my_titles_lis[i-2].getBoundingClientRect();
       var current_title = my_titles_lis[i].getBoundingClientRect();
-      if (current_title.top == previous_title.top) {
+      if (current_title.top == previous_title.top) { //if on the same line
+        // Inserts hyphen
         var hifen = document.createTextNode(" - ");
         my_titles_ul.insertBefore(hifen, my_titles_lis[i]);
         i += 1;
+
+        // Check if the inserted hyphen has created a line break
+        var current_title = my_titles_lis[i].getBoundingClientRect();
+        if (current_title.top != previous_title.top) {
+          my_titles_ul.removeChild(my_titles_lis[i-1]); //remove hyphen
+
+          // Inserts line break
+          line_break = document.createElement("br");
+          my_titles_ul.insertBefore(line_break, my_titles_lis[i-1]);
+        }
       }
     }
   } else {
